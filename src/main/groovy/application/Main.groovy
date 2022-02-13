10e335b3876f59b58b947c81cf311440cd05c6c2
bennoloeffler <benno.loeffler@gmx.de>
2022-02-13 17:13:31 +0100

    delivery-date-without-save

--- src/main/groovy/application/Main.groovy
+++ src/main/groovy/application/Main.groovy
@@ -22,7 +22,7 @@ import java.awt.Desktop
  */
 class  Main {
 
-    static VERSION_STRING ='1.4.0-ALPHA-Gpars-and-DeliveryDate'
+    static VERSION_STRING ='1.4.2-ALPHA-Gpars-and-DeliveryDate-WITHOUT-SAVE'
 
     def singleRunMode = false // instead: Deamon is default
     def multiInstanceMode = false // instead SingleInstance is default
