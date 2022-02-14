1f196b95bfa81a0e43e36d3bb0770700cf0f3aed
Benno Löffler <benno.loeffler@gmx.de>
2022-02-14 21:16:11 +0100

    1.5 average load

--- src/main/groovy/application/Main.groovy
+++ src/main/groovy/application/Main.groovy
@@ -22,7 +22,7 @@ import java.awt.Desktop
  */
 class  Main {
 
-    static VERSION_STRING ='1.4.4-delivery-date-macos'
+    static VERSION_STRING ='1.5.0-ALPHA-average-load'
 
     def singleRunMode = false // instead: Deamon is default
     def multiInstanceMode = false // instead SingleInstance is default
