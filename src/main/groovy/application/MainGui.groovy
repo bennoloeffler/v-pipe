06833a00a3d565cb704c15081aa1896a2a144ebc
bennoloeffler <benno.loeffler@gmx.de>
2022-02-27 11:35:18 +0100

    open bsp-daten in dev mode - not in release mode

--- src/main/groovy/application/MainGui.groovy
+++ src/main/groovy/application/MainGui.groovy
@@ -182,10 +182,13 @@ class MainGui {
         // start EDT and init model
         //
         view.start {
-            // bsp-daten/bsp-04-szenario-aus-verschiebung-und-template
-            //String dirToOpen ="./bsp-daten"
-            String dirToOpen ="./bsp-daten/bsp-04-szenario-aus-verschiebung-und-template"
-            controller.openDir(dirToOpen)
+            String dirToOpen ="./bsp-daten"
+            String currentStartPath = new File(".").absolutePath
+            if (currentStartPath.contains("projects")) {
+                // if in development mode
+                dirToOpen ="./bsp-daten/bsp-04-szenario-aus-verschiebung-und-template"
+                controller.openDir(dirToOpen)
+            }
             model.currentDir = dirToOpen
         }
 
