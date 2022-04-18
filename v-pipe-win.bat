07e043ebd929001efd4f6193b3238e1afda177e2
Benno Löffler <benno.loeffler@gmx.de>
2022-04-18 19:16:29 +0200

    win bat file works

--- v-pipe-win.bat
+++ v-pipe-win.bat
@@ -1,8 +1,8 @@
-set ABSPATH = %CD%
-echo "starting v-pipe here:"
+@echo off
+set ABSPATH=%CD%
+echo starting v-pipe here:
 echo %ABSPATH%
-cd "$ABSPATH"
-set JAVA_HOME=$ABSPATH/jre
-"$JAVA_HOME/bin/java" -cp "$ABSPATH/lib/*": application.MainGui
-
-java -Dfile.encoding=UTF-8 -Dlog4j.configurationFile=log4j2.xml -cp "./cream2.jar;./libs/*"   bel.cream2.deamon.CreamDeamonStarter
+cd "%ABSPATH%"
+set JAVA_HOME=%ABSPATH%/jre
+"%JAVA_HOME%/bin/java" -cp "%ABSPATH%/lib/*" application.MainGui
+rem -Dfile.encoding=UTF-8 --- app needs to be compiled with utf-8 (default win = windows-1252)
