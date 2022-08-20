11bf685de1252d713aa774a5305f6198e7c1abde
bennoloeffler <benno.loeffler@gmx.de>
2022-08-20 23:40:57 +0200

    run with timeout

--- git_pull_continously.groovy
+++ git_pull_continously.groovy
@@ -3,15 +3,19 @@ import java.util.concurrent.TimeUnit
 while (true) {
     def gitText = "git pull".execute().text.trim()
     println "Pulled last time at: " + new Date()
+    println deploy()
     if (gitText == "Already up to date.") {
         println gitText
-        Thread.sleep(1 * 60 * 1000)
+        Thread.sleep(30 * 1000)
     } else {
         println gitText
-        def proc = "gradle __deployToDropbox".execute()
-        proc.waitFor(5, TimeUnit.MINUTES)
-        def gradleText = proc.text.trim()
-        println gradleText
-
+        println deploy()
     }
+}
+
+String deploy() {
+    def proc = "gradle __deployToDropbox".execute()
+    proc.waitFor(5, TimeUnit.MINUTES)
+    def gradleText = proc.text.trim()
+    gradleText
 }
