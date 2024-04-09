c291df8e1039e23abc98f8987f8ff5f393ef838e
bennoloeffler <benno.loeffler@gmx.de>
2024-04-09 09:16:26 +0200

    fixed github pages fixed help to point to github-pages

--- src/main/groovy/application/MainGui.groovy
+++ src/main/groovy/application/MainGui.groovy
@@ -313,7 +313,7 @@ class MainGui {
             //path.replace(' ', '%20')
             //path = java.net.URLEncoder.encode(path, "UTF-8")
             //Desktop.getDesktop().browse(new URI("file:/$path"))
-            Desktop.getDesktop().browse(new URI("https://github.com/bennoloeffler/v-pipe/blob/master/Readme.md"))
+            Desktop.getDesktop().browse(new URI("https://bennoloeffler.github.io/v-pipe/"))
         }
     }
 }
